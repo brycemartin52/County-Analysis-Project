f062aaf25fe02e134f986c5f459d86d6cd6e2291
Bryce Martin <brycemartin52@gmail.com>
2023-12-14 21:10:20 -0700

    merge cleaning py files

--- county_package/clean_data.py
+++ county_package/clean_data.py
@@ -1,5 +1,5 @@
 """
-This script  cleans up the county voting data
+This script  cleans up the county voting, education, and cost of living data sets
 """
 
 if __name__ == "__main__":
@@ -45,3 +45,79 @@ if __name__ == "__main__":
     vcounty = vcounty.applymap(lambda s: s.title() if type(s) == str else s)
 
     vcounty.to_csv("../Data/viz_data.csv", index=False)
+
+
+    edu = pd.read_excel("../Data/Education.xlsx", header=3)
+
+    edu = edu[edu["State"] != "PR"]
+
+    edu = edu[
+        [
+            "State",
+            "Area name",
+            "Percent of adults with less than a high school diploma, 2017-21",
+            "Percent of adults with a high school diploma only, 2017-21",
+            "Percent of adults completing some college or associate's degree, 2017-21",
+            "Percent of adults with a bachelor's degree or higher, 2017-21",
+        ]
+    ]
+    edu.columns = ["State", "county_name", "noHS", "HS", "someCol", "Col"]
+
+    # Read the kaggle data
+    kag = pd.read_csv("../Data/cost_of_living_us.csv")
+    #kag
+
+    # Merge the data
+    merge1 = pd.merge(
+        kag,
+        edu,
+        left_on=["county", "state"],
+        right_on=["county_name", "State"],
+        how="inner",
+    )
+    merge1 = merge1.drop(columns=["State", "county_name"])
+    merge1.county = merge1.county.str.replace(" County", " ")
+    merge1.county = merge1.county.str.replace(" Parish", " ")
+    merge1.county = merge1.county.str.strip()
+    #merge1.info()
+
+    # State Abbreviation
+    abs = pd.read_csv("../Data/StateAbbvs.csv", header=None)
+    abs.columns = ["State", "Abv"]
+    abs.State = abs.State.str.upper()
+    #abs
+
+    # County data
+    vcounty = pd.read_csv("../Data/vcounty.csv")
+    vcounty = vcounty[
+        [
+            "state",
+            "county_name",
+            "totalvotes",
+            "DEMOCRAT",
+            "GREEN",
+            "LIBERTARIAN",
+            "OTHER",
+            "REPUBLICAN",
+        ]
+    ]
+
+    vcounty = pd.merge(vcounty, abs, left_on="state", right_on="State", how="inner")
+    vcounty = vcounty.drop(columns=["State"])
+
+    vcounty.Abv = vcounty.Abv.str.strip()
+    vcounty.county_name = vcounty.county_name.str.strip()
+    #vcounty.info()
+
+    # Final merge
+    final = pd.merge(
+        merge1,
+        vcounty,
+        left_on=["state", "county"],
+        right_on=["Abv", "county_name"],
+        how="inner",
+    )
+    #final.info()
+
+    # Write the final csv
+    final.to_csv("../Data/countyVLivEdu.csv")
