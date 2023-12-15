0f212061d2693716cc031ca997b7a818a8552669
Bryce Martin <brycemartin52@gmail.com>
2023-12-15 13:30:27 -0700

    trim cleaning file, moving extraneous data files

--- county_package/EDA.py
+++ county_package/EDA.py
@@ -14,34 +14,39 @@ import json
 # import pkg_resources
 from urllib.request import urlopen
 
-# Read the csv
-data = pd.read_csv("../Data/countyVLivEdu.csv")
-
-# pkg_resources... ('modules', '../Data/cou...')
-
-
-def plurality(row):
-    """
-    Plurality documentation
-    """
-    # Calculate percentage for each column
-    percentages = row / row.sum() * 100
-    # Get the party with the highest percentage
-    return percentages.idxmax()
-
 
 if __name__ == '__main__':
+    # Reading in necessary dataframes
+    with urlopen(
+        "https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json"
+    ) as response:
+        counties = json.load(response)
+
+    unemployment = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv", dtype={"fips": str})
+
+    fips_df = pd.read_csv("../Data/viz_data.csv", dtype={"county_fips": str})
+
+    # Tidying portion of clean_data.py
+    # Read the csv
+    data = pd.read_csv("../Data/countyVLivEdu.csv")
+    def plurality(row):
+        """
+        Plurality documentation
+        """
+        # Calculate percentage for each column
+        percentages = row / row.sum() * 100
+        # Get the party with the highest percentage
+        return percentages.idxmax()
+
 
     # Create a new column containing the most voted party for each row
     data["most_voted_party"] = data[
         ["REPUBLICAN", "DEMOCRAT", "LIBERTARIAN", "GREEN", "OTHER"]
     ].apply(plurality, axis=1)
+
     data["common_education"] = data[["noHS", "HS", "someCol", "Col"]].apply(
         plurality, axis=1
     )
-
-    # Display the updated DataFrame
-    data[["noHS", "HS", "someCol", "Col", "common_education"]]
 
 
     # data[data['common_education'] == 'noHS']
@@ -62,6 +67,9 @@ if __name__ == '__main__':
         }
     )
 
+
+
+    #scatterplot graphs
     sns.scatterplot(
         x=data.groupby(["county", "state_x"])["total_cost"].mean(),
         y=data.groupby(["county", "state_x"])["median_family_income"].mean(),
@@ -84,33 +92,36 @@ if __name__ == '__main__':
     plt.savefig("../Images/incomeVSfamilySize.png")
 
 
-    data_piv = pd.pivot_table(
+
+
+
+    cost_piv = pd.pivot_table(
         data,
         index=["county", "state_y", "median_family_income"],
         columns="family_member_count",
         values=["total_cost"],
     )
 
-    data_piv = data_piv.droplevel(0, axis=1).reset_index()
-    data_piv["median_family_cost"] = np.median(
-        data_piv[
+    cost_piv = cost_piv.droplevel(0, axis=1).reset_index()
+    cost_piv["median_family_cost"] = np.median(
+        cost_piv[
             ["1p0c", "1p1c", "1p2c", "1p3c", "1p4c", "2p0c", "2p1c", "2p2c", "2p3c", "2p4c"]
         ],
         axis=1,
     )
 
-    data_piv["income_cost_diff"] = data_piv["median_family_income"] - data_piv["2p1c"]
+    cost_piv["income_cost_diff"] = cost_piv["median_family_income"] - cost_piv["2p1c"]
 
-    fips_df = pd.read_csv("../Data/viz_data.csv", dtype={"county_fips": str})
+    
     fips_df.state = fips_df.state.str.upper()
-    data_piv = pd.merge(
-        data_piv,
+    cost_piv = pd.merge(
+        cost_piv,
         fips_df,
         left_on=["county", "state_y"],
         right_on=["county_name", "state"],
         how="inner",
     )
-    data_piv
+    cost_piv
 
 
     housing_piv = pd.pivot_table(
@@ -132,7 +143,7 @@ if __name__ == '__main__':
         housing_piv["median_family_income"] - housing_piv["2p1c"]
     )
 
-    fips_df = pd.read_csv("../Data/viz_data.csv", dtype={"county_fips": str})
+    
     fips_df.state = fips_df.state.str.upper()
     housing_piv = pd.merge(
         housing_piv,
@@ -143,14 +154,9 @@ if __name__ == '__main__':
     )
     housing_piv
 
-    with urlopen(
-        "https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json"
-    ) as response:
-        counties = json.load(response)
-
     ### Graph data on a map
     income = px.choropleth(
-        data_piv,
+        cost_piv,
         geojson=counties,
         locations="county_fips",
         color="median_family_income",
@@ -165,7 +171,7 @@ if __name__ == '__main__':
 
     ### Graph data on a map
     cost = px.choropleth(
-        data_piv,
+        cost_piv,
         geojson=counties,
         locations="county_fips",
         color="2p1c",
@@ -180,7 +186,7 @@ if __name__ == '__main__':
 
     ### Graph data on a map
     diff = px.choropleth(
-        data_piv,
+        cost_piv,
         geojson=counties,
         locations="county_fips",
         color="income_cost_diff",
@@ -193,15 +199,10 @@ if __name__ == '__main__':
     diff.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
     diff.show()
 
-    with urlopen(
-        "https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json"
-    ) as response:
-        counties = json.load(response)
 
-    vot = pd.read_csv("../Data/viz_data.csv", dtype={"county_fips": str})
 
     voting_fig = px.choropleth(
-        vot,
+        fips_df,
         geojson=counties,
         locations="county_fips",
         color="REPUBLICAN",
@@ -215,7 +216,7 @@ if __name__ == '__main__':
     voting_fig.show()
 
     fig2 = px.choropleth(
-        vot,
+        fips_df,
         geojson=counties,
         locations="county_fips",
         color="totalvotes",
@@ -229,7 +230,8 @@ if __name__ == '__main__':
     fig2.show()
 
 
-    unemployment = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv", dtype={"fips": str})
+
+
 
     unemp_chart = px.choropleth(unemployment, geojson=counties, locations='fips', color='unemp',
                             color_continuous_scale="Viridis",
