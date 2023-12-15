c2946710b0588845834979de3c3eb5fc8ef3eea0
Bryce Martin <brycemartin52@gmail.com>
2023-12-15 12:48:59 -0700

    add combined file

--- county_package/EDA.py
+++ county_package/EDA.py
@@ -189,3 +189,49 @@ if __name__ == '__main__':
     )
     diff.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
     diff.show()
+
+    with urlopen(
+        "https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json"
+    ) as response:
+        counties = json.load(response)
+
+    vot = pd.read_csv("../Data/viz_data.csv", dtype={"county_fips": str})
+
+    voting_fig = px.choropleth(
+        vot,
+        geojson=counties,
+        locations="county_fips",
+        color="REPUBLICAN",
+        color_continuous_scale="balance",
+        range_color=(0, 1),
+        scope="usa",
+        labels={"Republican": "% Republican"},
+    )
+    voting_fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    voting_fig.show()
+
+    fig2 = px.choropleth(
+        vot,
+        geojson=counties,
+        locations="county_fips",
+        color="totalvotes",
+        color_continuous_scale="Viridis",
+        hover_data=["state", "county_name"],
+        range_color=(0, 50000),
+        scope="usa",
+        labels={"totalvotes": "Voting population"},
+    )
+    fig2.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    fig2.show()
+
+
+    unemployment = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv", dtype={"fips": str})
+
+    unemp_chart = px.choropleth(unemployment, geojson=counties, locations='fips', color='unemp',
+                            color_continuous_scale="Viridis",
+                            range_color=(0, 12),
+                            scope="usa",
+                            labels={'unemp':'% Unemployment'}
+                            )
+    unemp_chart.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    unemp_chart.show()
