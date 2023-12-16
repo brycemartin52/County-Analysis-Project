a38336933eae6a039983976cd4c622cd6ff621f4
Michael Miceli <ilecimleahcim@gmail.com>
2023-12-15 20:00:09 -0700

    add pkg paths and defs for graphs in eda

--- county_package/EDA.py
+++ county_package/EDA.py
@@ -11,6 +11,7 @@ import plotly.express as px
 import json
 # import pkg_resources
 from urllib.request import urlopen
+import pkg_resources
 
 # Reading in necessary dataframes
 with urlopen(
@@ -18,14 +19,18 @@ with urlopen(
 ) as response:
     counties = json.load(response)
 
+
+
 unemployment = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv", dtype={"fips": str})
 
-fips_df = pd.read_csv("../Data/vcounty.csv", dtype={"county_fips": str})
+path_to_df = pkg_resources.resource_filename('County-Analysis-Project', '../Data/vcounty.csv')
+fips_df = pd.read_csv(path_to_df, dtype={"county_fips": str})
 
 
 # Tidying portion of clean_data.py
 # Read the csv
-final = pd.read_csv("../Data/countyVLivEdu.csv")
+path_to_final_df = pkg_resources.resource_filename('County-Analysis-Project', "../Data/countyVLivEdu.csv")
+final = pd.read_csv(path_to_final_df)
 def plurality(row):
     """
     Plurality documentation
@@ -51,7 +56,8 @@ final["most_voted_party"].value_counts()
 final["common_education"].value_counts()
 # data
 
-final.agg(
+def stat_sum():
+    final.agg(
     {
         "total_cost": ["min", "max", "median", "mean"],
         "median_family_income": ["min", "max", "median", "mean"],
@@ -62,7 +68,7 @@ final.agg(
         "someCol": ["min", "max", "median", "mean"],
         "Col": ["min", "max", "median", "mean"],
     }
-)
+    )
 
 
 cost_piv = pd.pivot_table(
@@ -70,7 +76,7 @@ cost_piv = pd.pivot_table(
     index=["county", "state_y", "median_family_income"],
     columns="family_member_count",
     values=["total_cost"],
-)
+ )
 
 cost_piv = cost_piv.droplevel(0, axis=1).reset_index()
 
@@ -91,7 +97,7 @@ cost_piv = pd.merge(
     left_on=["county", "state_y"],
     right_on=["county_name", "state"],
     how="inner",
-)
+)   
 
 housing_piv = pd.pivot_table(
     final,
@@ -113,7 +119,6 @@ housing_piv["income_cost_diff"] = (
 )
 
 fips_df.state = fips_df.state.str.title()
-
 housing_piv = pd.merge(
     housing_piv,
     fips_df,
@@ -125,112 +130,117 @@ housing_piv = pd.merge(
 
 
 ### Scatterplot graphs
-sns.scatterplot(
-    x=final.groupby(["county", "state_x"])["total_cost"].mean(),
-    y=final.groupby(["county", "state_x"])["median_family_income"].mean(),
-    hue=final.groupby(["county", "state_x"])["common_education"].first(),
-)
-plt.savefig("../Images/incomeVSeducation.png")
+def edu_scat():
+    sns.scatterplot(
+        x=final.groupby(["county", "state_x"])["total_cost"].mean(),
+        y=final.groupby(["county", "state_x"])["median_family_income"].mean(),
+        hue=final.groupby(["county", "state_x"])["common_education"].first(),
+    )
+    plt.savefig("../Images/incomeVSeducation.png")
 
-sns.scatterplot(
-    x=final.groupby(["county", "state_x"])["total_cost"].mean(),
-    y=final.groupby(["county", "state_x"])["median_family_income"].mean(),
-    hue=final.groupby(["county", "state_x"])["most_voted_party"].first(),
-)
-plt.savefig("../Images/incomeVSvoting.png")
+def vote_scat():
+    sns.scatterplot(
+        x=final.groupby(["county", "state_x"])["total_cost"].mean(),
+        y=final.groupby(["county", "state_x"])["median_family_income"].mean(),
+        hue=final.groupby(["county", "state_x"])["most_voted_party"].first(),
+    )
+    plt.savefig("../Images/incomeVSvoting.png")
 
-sns.scatterplot(
-    x=final["total_cost"],
-    y=final["median_family_income"],
-    hue=final["family_member_count"],
-)
-plt.savefig("../Images/incomeVSfamilySize.png")
+def family_size_scat():
+    sns.scatterplot(
+        x=final["total_cost"],
+        y=final["median_family_income"],
+        hue=final["family_member_count"],
+    )
+    plt.savefig("../Images/incomeVSfamilySize.png")
+
+### Graph data on a map
+def income_map():
+    income = px.choropleth(
+        cost_piv,
+        geojson=counties,
+        locations="county_fips",
+        color="median_family_income",
+        color_continuous_scale="balance",
+        hover_data=["state", "county_name"],
+        range_color=(30000, 100000),
+        scope="usa",
+        labels={"median_family_income": "Median Income"},
+    )
+    income.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    income.show()
 
 
 ### Graph data on a map
-income = px.choropleth(
-    cost_piv,
-    geojson=counties,
-    locations="county_fips",
-    color="median_family_income",
-    color_continuous_scale="balance",
-    hover_data=["state", "county_name"],
-    range_color=(30000, 100000),
-    scope="usa",
-    labels={"median_family_income": "Median Income"},
-)
-income.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-income.show()
+def cost_map():
+    cost = px.choropleth(
+        cost_piv,
+        geojson=counties,
+        locations="county_fips",
+        color="2p1c",
+        color_continuous_scale="balance",
+        hover_data=["state", "county_name"],
+        range_color=(30000, 100000),
+        scope="usa",
+        labels={"2p1c": "2p1c Cost"},
+    )
+    cost.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    cost.show()
 
 
 ### Graph data on a map
-cost = px.choropleth(
-    cost_piv,
-    geojson=counties,
-    locations="county_fips",
-    color="2p1c",
-    color_continuous_scale="balance",
-    hover_data=["state", "county_name"],
-    range_color=(30000, 100000),
-    scope="usa",
-    labels={"2p1c": "2p1c Cost"},
-)
-cost.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-cost.show()
+def income_cost_diff_map():
+    diff = px.choropleth(
+        cost_piv,
+        geojson=counties,
+        locations="county_fips",
+        color="income_cost_diff",
+        color_continuous_scale="RdBu",
+        hover_data=["state", "county_name"],
+        range_color=(-55000, 55000),
+        scope="usa",
+        labels={"income_cost_diff": "Median income - cost"},
+    )
+    diff.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    diff.show()
+
+def Republican_map():
+    voting_fig = px.choropleth(
+        fips_df,
+        geojson=counties,
+        locations="county_fips",
+        color="REPUBLICAN",
+        color_continuous_scale="balance",
+        hover_data=["state", "county_name"],
+        range_color=(0, 1),
+        scope="usa",
+        labels={"Republican": "% Republican"},
+    )
+    voting_fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    voting_fig.show()
+
+def vote_map():
+    fig2 = px.choropleth(
+        fips_df,
+        geojson=counties,
+        locations="county_fips",
+        color="totalvotes",
+        color_continuous_scale="Viridis",
+        hover_data=["state", "county_name"],
+        range_color=(0, 100000),
+        scope="usa",
+        labels={"totalvotes": "Voting population"},
+    )
+    fig2.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    fig2.show()
 
 
-### Graph data on a map
-diff = px.choropleth(
-    cost_piv,
-    geojson=counties,
-    locations="county_fips",
-    color="income_cost_diff",
-    color_continuous_scale="RdBu",
-    hover_data=["state", "county_name"],
-    range_color=(-55000, 55000),
-    scope="usa",
-    labels={"income_cost_diff": "Median income - cost"},
-)
-diff.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-diff.show()
-
-
-voting_fig = px.choropleth(
-    fips_df,
-    geojson=counties,
-    locations="county_fips",
-    color="REPUBLICAN",
-    color_continuous_scale="balance",
-    hover_data=["state", "county_name"],
-    range_color=(0, 1),
-    scope="usa",
-    labels={"Republican": "% Republican"},
-)
-voting_fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-voting_fig.show()
-
-
-fig2 = px.choropleth(
-    fips_df,
-    geojson=counties,
-    locations="county_fips",
-    color="totalvotes",
-    color_continuous_scale="Viridis",
-    hover_data=["state", "county_name"],
-    range_color=(0, 100000),
-    scope="usa",
-    labels={"totalvotes": "Voting population"},
-)
-fig2.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-fig2.show()
-
-
-
-unemp_chart = px.choropleth(unemployment, geojson=counties, locations='fips', color='unemp',
-                        color_continuous_scale="Viridis",
-                        range_color=(0, 12),
-                        scope="usa",
-                        labels={'unemp':'% Unemployment'}
-                        )
-unemp_chart.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-unemp_chart.show()
+def unemployement_map():
+    unemp_chart = px.choropleth(unemployment, geojson=counties, locations='fips', color='unemp',
+                            color_continuous_scale="Viridis",
+                            range_color=(0, 12),
+                            scope="usa",
+                            labels={'unemp':'% Unemployment'}
+                            )
+    unemp_chart.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    unemp_chart.show()
